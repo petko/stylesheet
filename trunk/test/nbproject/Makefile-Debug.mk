78d92edc5abadb0d0e13850e661fab2ae87e6acd
marco.ambu <marco.ambu@db928998-1a79-11df-b1cb-2be540f5faec>
2010-04-08 15:07:24 +0000

    Added some more tests; splitted single test file into multiple files.

--- trunk/test/nbproject/Makefile-Debug.mk
+++ trunk/test/nbproject/Makefile-Debug.mk
@@ -31,7 +31,11 @@ OBJECTDIR=build/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
-	${OBJECTDIR}/src/TestStyleSheet.o
+	${OBJECTDIR}/src/TestElement.o \
+	${OBJECTDIR}/src/TestSelector.o \
+	${OBJECTDIR}/src/TestStyleSheet.o \
+	${OBJECTDIR}/src/TestParser.o \
+	${OBJECTDIR}/src/TestProperty.o
 
 # C Compiler Flags
 CFLAGS=
@@ -57,10 +61,30 @@ dist/Debug/GNU-Linux-x86/test: ${OBJECTFILES}
 	${MKDIR} -p dist/Debug/GNU-Linux-x86
 	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/test ${OBJECTFILES} ${LDLIBSOPTIONS} 
 
+${OBJECTDIR}/src/TestElement.o: nbproject/Makefile-${CND_CONF}.mk src/TestElement.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} $@.d
+	$(COMPILE.cc) -g -D_DEBUG -I../include -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/TestElement.o src/TestElement.cpp
+
+${OBJECTDIR}/src/TestSelector.o: nbproject/Makefile-${CND_CONF}.mk src/TestSelector.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} $@.d
+	$(COMPILE.cc) -g -D_DEBUG -I../include -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/TestSelector.o src/TestSelector.cpp
+
 ${OBJECTDIR}/src/TestStyleSheet.o: nbproject/Makefile-${CND_CONF}.mk src/TestStyleSheet.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} $@.d
 	$(COMPILE.cc) -g -D_DEBUG -I../include -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/TestStyleSheet.o src/TestStyleSheet.cpp
+
+${OBJECTDIR}/src/TestParser.o: nbproject/Makefile-${CND_CONF}.mk src/TestParser.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} $@.d
+	$(COMPILE.cc) -g -D_DEBUG -I../include -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/TestParser.o src/TestParser.cpp
+
+${OBJECTDIR}/src/TestProperty.o: nbproject/Makefile-${CND_CONF}.mk src/TestProperty.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} $@.d
+	$(COMPILE.cc) -g -D_DEBUG -I../include -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/TestProperty.o src/TestProperty.cpp
 
 # Subprojects
 .build-subprojects:
