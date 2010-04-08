8523912c915b199c7cef6f2fb58cd08547833d8b
marco.ambu <marco.ambu@db928998-1a79-11df-b1cb-2be540f5faec>
2010-04-08 15:02:43 +0000

    CssSelector::getName returns const std::string&

--- trunk/include/StyleSheet/Selector.h
+++ trunk/include/StyleSheet/Selector.h
@@ -30,87 +30,86 @@ namespace StyleSheet {
 class CssSelector
 {
 public:
-    // static ctors
-    static CssSelector CssTypeSelector(const std::string& name)
-    {
-        if (name.empty())
-            throw std::invalid_argument("empty CSS selector");
-        return CssSelector(SELECTOR_TYPE, name);
-    }
-    static CssSelector CssClassSelector(const std::string& name)
-    {
-        if (name.empty())
-            throw std::invalid_argument("empty CSS selector");
-        return CssSelector(SELECTOR_CLASS, name);
-    }
-    static CssSelector CssIdSelector(const std::string& name)
-    {
-        if (name.empty())
-            throw std::invalid_argument("empty CSS selector");
-        return CssSelector(SELECTOR_ID, name);
-    }
-    static CssSelector parse(const std::string& str)
-    {
-        std::string selector = str;
-        boost::algorithm::trim(selector);
+  // static ctors
+  static CssSelector CssTypeSelector(const std::string& name)
+  {
+    if (name.empty())
+      throw std::invalid_argument("empty CSS selector");
+    return CssSelector(SELECTOR_TYPE, name);
+  }
+  static CssSelector CssClassSelector(const std::string& name)
+  {
+    if (name.empty())
+      throw std::invalid_argument("empty CSS selector");
+    return CssSelector(SELECTOR_CLASS, name);
+  }
+  static CssSelector CssIdSelector(const std::string& name)
+  {
+    if (name.empty())
+      throw std::invalid_argument("empty CSS selector");
+    return CssSelector(SELECTOR_ID, name);
+  }
+  static CssSelector parse(const std::string& str)
+  {
+    std::string selector = str;
+    boost::algorithm::trim(selector);
 
-        if (selector.empty())
-            throw std::invalid_argument("empty CSS selector");
+    if (selector.empty())
+      throw std::invalid_argument("empty CSS selector");
 
-        if (selector[0] == '.')
-            return CssSelector(SELECTOR_CLASS, selector.substr(1));
+    if (selector[0] == '.')
+      return CssSelector(SELECTOR_CLASS, selector.substr(1));
 
-        if (selector[0] == '#')
-            return CssSelector(SELECTOR_ID, selector.substr(1));
-		
-        return CssSelector(SELECTOR_TYPE, selector);
-    }
+    if (selector[0] == '#')
+      return CssSelector(SELECTOR_ID, selector.substr(1));
+
+    return CssSelector(SELECTOR_TYPE, selector);
+  }
 
 public:
-    bool operator==(const CssSelector& other) const { return type_ == other.type_ && name_ == other.name_; }
-    bool operator!=(const CssSelector& other) const { return !(*this == other); }
+  bool operator==(const CssSelector& other) const { return type_ == other.type_ && name_ == other.name_; }
+  bool operator!=(const CssSelector& other) const { return !(*this == other); }
 
-    bool operator<(const CssSelector& s) const {
-        return type_ < s.type_ || (type_ == s.type_ && name_ < s.name_);
-    }
+  bool operator<(const CssSelector& s) const {
+    return type_ < s.type_ || (type_ == s.type_ && name_ < s.name_);
+  }
 
-    const std::string getName() const { return name_; }
+  const std::string& getName() const { return name_; }
 
-    bool isType() const { return type_ == SELECTOR_TYPE; }
-    bool isClass() const { return type_ == SELECTOR_CLASS; }
-    bool isId() const { return type_ == SELECTOR_ID; }
+  bool isType() const { return type_ == SELECTOR_TYPE; }
+  bool isClass() const { return type_ == SELECTOR_CLASS; }
+  bool isId() const { return type_ == SELECTOR_ID; }
 
-    std::string toString() const { return typeToString() + name_; }
-
-private:
-    enum Type {
-        //SELECTOR_BAD,
-        SELECTOR_TYPE,
-        SELECTOR_CLASS,
-        SELECTOR_ID
-    };
+  std::string toString() const { return typeToString() + name_; }
 
 private:
-    CssSelector(Type type, const std::string& name)
-        : type_(type), name_(name)
-    {}
+  enum Type {
+    SELECTOR_TYPE,
+    SELECTOR_CLASS,
+    SELECTOR_ID
+  };
 
 private:
-    std::string typeToString() const
+  CssSelector(Type type, const std::string& name)
+    : type_(type), name_(name)
+  {}
+
+private:
+  std::string typeToString() const
+  {
+    switch (type_)
     {
-        switch (type_)
-        {
-        case SELECTOR_TYPE: return "";
-        case SELECTOR_CLASS: return ".";
-        case SELECTOR_ID: return "#";
-        default:
-            throw std::runtime_error("Bad CssSelector type");
-        }
+    case SELECTOR_TYPE: return "";
+    case SELECTOR_CLASS: return ".";
+    case SELECTOR_ID: return "#";
+    default:
+      throw std::runtime_error("Bad CssSelector type");
     }
+  }
 
 private:
-    Type type_;
-    std::string name_;
+  Type type_;
+  std::string name_;
 };
 
 } // namespace StyleSheet
